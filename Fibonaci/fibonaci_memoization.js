6567b1063d21f4a7969d5781e2be5a9e502b3acb
sean miller <>
2019-03-19 20:47:55 -0700

    Fibonaci with tabulation

--- Fibonaci/fibonaci_memoization.js
+++ Fibonaci/fibonaci_memoization.js
@@ -3,12 +3,10 @@ function fib(n, memo=[]) {
   if(n <=2 ) return 1;
   var res = fib(n-1, memo) + fib(n-2, memo);
   memo[n] = res;
-  console.log("RES IS:", res);
   return res;
 }
 
 fib(10);
 
-
 // SPACE COMPLEXITY
-// TIME COMPLEXITY
+// TIME COMPLEXITY - O(N)
---
+++ Fibonaci/fibonaci_tabulation.js
@@ -0,0 +1,13 @@
+function fib(n) {
+  if(n <= 2) return 1;
+  var sequence = [0,1,1];
+  for(var i = 3; i <= n; i++) {
+    sequence[i] = sequence[i-1] + sequence[i-2];
+  }
+  console.log(sequence);
+  return sequence;
+}
+
+fib(6);
+
+// TIME COMPLEXITY - O(N)
